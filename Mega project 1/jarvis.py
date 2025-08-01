db77d2c18f7eb1a1798f9c90a752215a9c7221f0
samirthapa7755 <158297027+samirthapa7755@users.noreply.github.com>
2025-08-01 09:23:29 +0545

    project added

---
+++ Mega project 1/jarvis.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+ttsx = pyttsx3.init()
+
+def speak(text):
+    engine.say("I will speak this text")
+    
+if __name__ == "__main__":
